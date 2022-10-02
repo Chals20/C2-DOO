a1b7c117a88b39ed6f8318729c48aeff756d6ef7
Chals <csanchezbasket@gmail.com>
2022-10-02 23:21:03 +0200

    Commit Tarea 1

---
+++ C2-DOO/src/tarea1/Tarea1.java
@@ -0,0 +1,21 @@
+package tarea1;
+
+public class Tarea1 {
+
+	public static void main(String[] args) {
+		
+		int a = 22;
+		int b = 11;
+		int resultado = a+b;
+		System.out.println("Suma: " + resultado);
+		resultado = a-b;
+		System.out.println("Resta: " + resultado);
+		resultado = a*b;
+		System.out.println("Multiplicación: " + resultado);
+		resultado = a/b;
+		System.out.println("División: " + resultado);
+		resultado = a%b;
+		System.out.println("Módulo: " + resultado);
+	}
+
+}
